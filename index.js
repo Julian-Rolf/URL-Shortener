e26572784e7ac8c9c8e0bcc3b05df57d8c64fb17
Julian Rolf <julian.rolf@yahoo.de>
2020-12-26 13:48:55 +0100

    finished basic functionality

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const nanoid = require('nanoid');
+const path = require('path');
+const pug = require('pug');
+const bodyParser = require('body-parser');
 const config = require('./config');
 
 // setup db
@@ -13,23 +16,34 @@ const collection = db.get('main');
 const app = express();
 app.use(morgan('tiny'));
 app.use(helmet());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'pug');
+app.set('views', './views');
 const port = config.port || 8080;
 
 app.get('/', (req, res) => {
-    res.send('Hello Test!');
+    res.render('index.pug');
 });
 
 app.post('/', (req, res) => {
     let { url } = req.body;
+    let slug = nanoid.nanoid(5);
+
+    collection.insert({ slug: slug, url: url });
+    res.send(`127.0.0.1/${slug}`);
 });
 
-app.get('/:id', (req, res) => {
-    let { id } = req.params;
-    console.log(id);
-
-    res.send('Worked');
+app.get('/:slug', (req, res) => {
+    let { slug } = req.params;
+    collection.findOne({ slug: slug }).then((doc) => {
+        res.redirect(doc.url);
+    }).catch((err) => {
+        res.sendStatus(404);
+    });
 });
 
 app.listen(port, () => {
     console.log(`Server started at http://localhost:${port}`);
 });
+
