eea2f2157136d0804f785ae4a55ae945afd645f9
Julian Rolf <julian.rolf@yahoo.de>
2020-12-27 22:15:29 +0100

    naming adjustments

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,27 +1,27 @@
 window.onload = () => {
     var form = document.getElementById('formId');
-    var input = document.getElementById('urlInput');
+    var input = document.getElementById('inputId');
 
     form.addEventListener('submit', function (event) {
         event.preventDefault();
         var data = { url: input.value };
-        check(data);
+        sendData(data);
     });
 };
 
-function check(data) {
+function sendData(data) {
     var request = new XMLHttpRequest();
     request.addEventListener('load', function (event) {
-        document.getElementById('btn').style.display = "none";
-        document.getElementById('urlInput').style.display = "none";
+        document.getElementById('btnId').style.display = "none";
+        document.getElementById('inputId').style.display = "none";
 
-        let label = document.getElementById('urlLabel');
+        let label = document.getElementById('labelId');
         label.style.border = "3px solid black";
         label.style.borderRadius = "15px";
         label.style.padding = "20px";
         label.innerHTML = JSON.parse(request.responseText).message;
     });
-    request.headers
+
     request.open("POST", "/");
     request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
     request.send(JSON.stringify(data));
